5dc1a0cb5f03e9cd695eb722be1a8049b174366c
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-03-13 13:06:18 -0300

    revert

--- src/middlewares/validUser.js
+++ src/middlewares/validUser.js
@@ -4,22 +4,21 @@ const { verify } = require('jsonwebtoken')
 function validUser(req, res, next) {
   try {
     const token = req.headers['authorization'];
-    if (!token) return res.status(400).send({ error: 'You must be logged in' })
+    if (!token) return res.send({ error: 'You must be logged in' })
     const bearerToken = token.split(' ')
-    if (bearerToken[0] !== 'Bearer') return res.status(400).send({ error: 'Invalid type Token' })
+    if (bearerToken[0] !== 'Bearer') return res.send({ error: 'Invalid type Token' })
 
     verify(bearerToken[1], process.env.SECRET, (e, data) => {
-      if (e) return res.status(400).send({ e })
-      if (!data) return res.status(400).send({ error: 'Invalid token' })
+      if (e) return res.send({ e })
+      if (!data) return res.send({ error: 'Invalid token' })
       else {
         next()
       }
     })
   } catch (error) {
-    return res.status(400).json(error);
+    res.send({ error })
   }
 }
-
 
 module.exports = {
   validUser
